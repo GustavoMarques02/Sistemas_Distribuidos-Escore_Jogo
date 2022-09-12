ffb2a5d1919adb032070c3b3171abe8af2372c8a
Gustavo Marques Reis <gustavo_marquesr@discente.ufg.br>
2022-09-12 17:25:19 -0300

    Create coordenador.py

---
+++ PP6/coordenador.py
@@ -0,0 +1,36 @@
+from socket import *
+import threading
+
+
+class Coordenador(threading.Thread):
+    def __init__(self, ip):
+        threading.Thread.__init__(self)
+        self.socketCliente = socket(AF_INET, SOCK_STREAM)
+        self.socketCliente.bind((ip, 6002))
+        self.socketCliente.listen(3)
+        self.pilha = []
+        self.lock = False
+        print('Coordenador Pronto')
+
+    def run(self):
+        while True:
+            (conn, addr) = self.socketCliente.accept()
+            threading.Thread(
+                target=self.__handling_connection(conn)).start()
+
+    def __handling_connection(self, conn):
+        id = conn.recv(1024)
+        if self.lock:
+            self.pilha.append(id)
+            print('Processo ' + str(id) + ' adicionado na pilha')
+            while True:
+                if id not in self.pilha:
+                    print('Processo ' + str(id) + ' saiu na pilha')
+                    break
+        self.lock = True
+        conn.send('OK'.encode())
+        conn.recv(1024)
+        conn.close()
+        self.lock = False
+        if len(self.pilha) != 0:
+            del self.pilha[0]
